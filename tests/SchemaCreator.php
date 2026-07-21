bfd770a0fc7e07069b084942e4239d154d6a522e
Gabriel Birke <gabriel.birke@wikimedia.de>
2026-07-21 12:23:49 +0200

    Update PHPStan to 2.x
    Update PHPStan and all plugins
    
    Fix type annotations in code and remove unnecessary checks.

--- tests/SchemaCreator.php
+++ tests/SchemaCreator.php
@@ -29,7 +29,7 @@ class SchemaCreator {
 	}
 
 	/**
-	 * @return array<int, ClassMetadata>
+	 * @return list<ClassMetadata<*>>
 	 */
 	private function getClassMetaData(): array {
 		return $this->entityManager->getMetadataFactory()->getAllMetadata();
